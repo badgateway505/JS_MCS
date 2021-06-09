01ebaaf7141e916e5cd5cd4ecb397a09d03b8b86
badgateway505 <82412738+badgateway505@users.noreply.github.com>
2021-06-09 23:38:35 +0300

    Update script.js

--- final-project/js/script.js
+++ final-project/js/script.js
@@ -1,6 +1,7 @@
 const APIkey = '423b5dee60e0f3b462bba311cbb37393';
 let url = 'https://api.openweathermap.org/data/2.5/weather?q=Москва&appid='+APIkey;
 
+//lol
 //отправление запроса
 let xhr = new XMLHttpRequest();
 xhr.open('GET', url, false);
